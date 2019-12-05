36319441da74f3d48c9536e0d0fc5b56942d5fb3
Jakub Buziewicz <buzekxd@rallycommunication.pl>
2019-12-05 18:10:59 +0100

    Ustawienie Hibernate i Gradle. Problem z dialectem...

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,26 @@
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
 public class Main {
+    private static SessionFactory sessionFactory = null;
+
+    public static void main(String[] args) {
+        sessionFactory = getSessionFactory();
+        Session session = sessionFactory.openSession();
+//        Transaction tx = session.beginTransaction();
+//        session.save();
+//        tx.commit();
+//        session.close();
+    }
+
+    private static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            Configuration configuration = new Configuration();
+            sessionFactory = configuration.configure().buildSessionFactory();
+        }
+        return sessionFactory;
+    }
 
 }
